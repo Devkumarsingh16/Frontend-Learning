696c894a68429e85c45c29be2c2d4924ecff8527
Dev Kumar Singh <sdev06829@gmail.com>
2026-09-04 22:44:05 +0530

    Implement conditional statements examples in 02_conditional.js
    Added examples of if-else, if-else if, and switch case statements.

---
+++ JavaScript/L03 Operator and Condititonal/02_conditional.js
@@ -0,0 +1,42 @@
+                                       // Topic:if-else
+let age = 25;
+
+if(age > 18){
+    console.log("can vote");
+
+}
+else{
+    console.log("can't vote");
+}
+
+                                         // Topic: if-else if
+let num = 3;
+
+if(num == 1){
+    console.log("a");
+}
+else if(num == 2){
+    console.log("b");
+}
+else if(num == 3){
+    console.log("c");
+}
+else{
+    console.log("none");
+}
+
+                                          // Topic: Switch case
+let num = 3;
+
+switch(num){
+    case 1:console.log("a");
+    break;
+    case 2:console.log("b");
+    break;
+    case 3:console.log("c");
+    break;
+    case 4:console.log("d");
+    break;
+    default:console.log("none");
+    
+}
