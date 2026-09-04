35055b78a14f4b3d928d28a42f482082176fe76e
Dev Kumar Singh <sdev06829@gmail.com>
2026-09-04 22:41:47 +0530

    Implement operator examples in 01_operator.js
    Added examples of arithmetic, equality, ternary, and logical operators in JavaScript.

---
+++ JavaScript/L03 Operator and Condititonal/01_operator.js
@@ -0,0 +1,32 @@
+// arithematic
+let a = 3;
+let b = 4;
+
+// console.log(a+b);
+
+                                      // Topic:loose equality
+// console.log("5" == 5);
+
+                                      // Topic:strict equality
+
+// console.log("5" === 5);  // because different datatype
+// console.log(5 === 5);
+
+
+                                         // Topic: Ternary operator
+
+let age = 25;
+let status1 = (age > 18)?"I can vote" : "I can't  vote"
+
+// console.log(status1);
+
+                                        // Topic: Logical operator
+
+let ans = (true && false);  // AND operator
+console.log(ans);
+
+let res = (true || false); // OR operator
+console.log(res);
+
+let output = !(true);      // NOT operator
+console.log(output);
